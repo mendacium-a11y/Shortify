57c65beb9bc5a3f0cb5e3c6a69fa69e6f4edc771
mendacium <sakshamlinkedin@gmail.com>
2024-02-28 20:44:57 +0530

    Add input component and update home page layout

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import Navbar from '@/components/Navbar'
+import { Input } from "@/components/ui/input"
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
     <>
-    <Navbar/>
-    <div className='h-[90vh] bg-slate-950'></div>
-    
+      <Navbar />
+      <div className='h-[90vh] bg-slate-950 flex justify-center items-start'>
+        <div className='text-white text-center mt-32 font-sans text-8xl'>
+          <p
+            className="text-6xl md:text-8xl font-extrabold bg-clip-text text-transparent bg-[linear-gradient(to_right,theme(colors.indigo.400),theme(colors.indigo.100),theme(colors.sky.400),theme(colors.fuchsia.400),theme(colors.sky.400),theme(colors.indigo.100),theme(colors.indigo.400))] bg-[length:200%_auto] animate-gradient">
+            The Fastest Way
+          </p>
+          <p 
+          className="text-6xl md:text-8xl font-extrabold bg-clip-text text-transparent bg-[linear-gradient(to_right,theme(colors.indigo.400),theme(colors.indigo.100),theme(colors.sky.400),theme(colors.fuchsia.400),theme(colors.sky.400),theme(colors.indigo.100),theme(colors.indigo.400))] bg-[length:200%_auto] animate-gradient">
+            To Shorten
+          </p>
+          <p 
+          className="text-6xl md:text-8xl font-extrabold bg-clip-text text-transparent bg-[linear-gradient(to_right,theme(colors.indigo.400),theme(colors.indigo.100),theme(colors.sky.400),theme(colors.fuchsia.400),theme(colors.sky.400),theme(colors.indigo.100),theme(colors.indigo.400))] bg-[length:200%_auto] animate-gradient">Links
+          </p>
+          <Input className='bg-slate-950 border-slate-700 focus:border-slate-500 border-4 mt-20 h-16 px-5' />
+          <Button className='rounded-3xl bg-violet-700 hover:bg-white text-white hover:text-black text-xl h-12 w-36'> Generate</Button>
+        </div>
+      </div>
+
     </>
   )
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -72,6 +72,9 @@ module.exports = {
         "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
+    fontFamily: {
+      sans: ["Kode Mono", "Montserrat"]
+    }
   },
   plugins: [require("tailwindcss-animate")],
 }
