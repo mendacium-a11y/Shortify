74af86df68ea830267c1e9a744734cbb21339d8e
mendacium <sakshamlinkedin@gmail.com>
2024-03-02 18:30:51 +0530

    Refactor code to improve performance and readability

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,9 @@
 import path from "path"
 import react from "@vitejs/plugin-react-swc"
-import million from "million/compiler";
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [million.vite({
-    auto: {
-      threshold: 0.05,
-      skip: ["useBadHook", /badVariable/g],
-    },
-  }),react()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
