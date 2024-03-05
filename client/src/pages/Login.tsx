17bcbb5c291a876a9837b5b69c8f653e9ec0df2e
mendacium <sakshamlinkedin@gmail.com>
2024-03-05 22:39:47 +0530

    Remove unused import in Login component

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export default function Login() {
   return (
     <div className="absolute inset-0 text-white h-full w-full px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)] flex justify-center items-center">
